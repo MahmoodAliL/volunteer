1320d3a7c0b671df693279d3b98386e178d4d253
MahmoodAliHasan <mahmoodg770099851g@gmail.com>
2018-02-06 16:42:17 +0300

    Delete SignupMvpPresenter.kt

--- app/src/main/java/com/teaml/iq/volunteer/ui/account/signup/SignupMvpPresenter.kt
+++
@@ -1,9 +0,0 @@
-package com.teaml.iq.volunteer.ui.account.signup
-
-import com.teaml.iq.volunteer.ui.base.MvpPresenter
-
-/**
- * Created by ali on 2/1/2018.
- */
-interface SignupMvpPresenter<V : SignupMvpView> : MvpPresenter<V> {
-}
